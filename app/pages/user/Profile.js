033bb2ea71acaf5b88758a41e1168f6024d595e9
guowei3 <1145165094@qq.com>
2017-03-23 21:34:26 +0800

    temp

--- app/pages/user/Profile.js
+++ app/pages/user/Profile.js
@@ -11,7 +11,7 @@ import {
   Alert,
 } from 'react-native';
 
-import Icon from 'react-native-vector-icons/Ionicons';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import styleConstant from '../../style/styleConstant';
 import styleProfile from '../../style/user/styleProfile';
 
@@ -54,11 +54,36 @@ class Register extends React.Component {
                 <TouchableOpacity onPress={this.onPress}  activeOpacity={0.8}>
                   <Image source={require('../../images/arrow_left.png')} style={styleProfile.backImage}></Image>
                 </TouchableOpacity>
-                <Text>梦梦</Text>
+                <Text style={styleProfile.profileName}>梦梦</Text>
+              </View>
+              <View style={styleProfile.profilePhoto}>
+                <Image source={require('../../images/meinv.png')} style={styleProfile.profile}></Image>
               </View>
             </Image>
           </View>
-
+          <View style={styleProfile.profileTitle}>
+            <Text style={styleProfile.profileText}>梦梦</Text>
+            <Icon.Button name="mars" color="#fff" style={styleProfile.gender} size={15} backgroundColor="#ff96aa">
+              <Text style={styleProfile.age}>24</Text>
+            </Icon.Button>
+          </View>
+          <View style={styleProfile.itemTitleWrap}>
+            <Text style={styleProfile.itemTitle}>我的信息</Text>
+          </View>
+          <View style={styleProfile.itemInfo}>
+            <View style={styleProfile.itemLi}>
+              <Text style={styleProfile.itemLabel}>星座</Text>
+              <Text style={styleProfile.itemValue}>金牛座</Text>
+            </View>
+            <View style={styleProfile.itemLi}>
+              <Text style={styleProfile.itemLabel}>职业</Text>
+              <Text style={styleProfile.itemValue}>政府机构</Text>
+            </View>
+            <View style={styleProfile.itemLi}>
+              <Text style={styleProfile.itemLabel}>城市</Text>
+              <Text style={styleProfile.itemValue}>四川省成都市</Text>
+            </View>
+          </View>
         </ScrollView>
       </View>
     );
--- app/style/user/styleProfile.js
+++ app/style/user/styleProfile.js
@@ -26,7 +26,79 @@ const styleProfile = StyleSheet.create({
   backImage:{
     width:26,
     height:26
+  },
+  profileName:{
+    color:'#fff',
+    marginLeft:30,
+    fontSize:18,
+    marginTop:0
+  },
+  profilePhoto:{
+    flexDirection:'row',
+    alignItems:'center',
+    justifyContent:'center',
+    marginTop:80
+  },
+  profile:{
+    width:80,
+    height:80,
+    borderRadius:40,
+    borderWidth:2,
+    borderColor:'#fff'
+  },
+  profileTitle:{
+    backgroundColor:'#fff',
+    flexDirection:'row',
+    paddingTop:10,
+    paddingBottom:10,
+    borderBottomWidth:1,
+    borderBottomColor:'#dbdbdb'
+  },
+  profileText:{
+    color:'#333',
+    fontSize:18,
+    paddingLeft:20,
+    marginRight:15,
+  },
+  gender:{
+    height:20,
+  },
+  age:{
+    color:'#fff',
+    marginLeft:-5
+  },
+  itemTitleWrap:{
+    paddingTop:12,
+    paddingBottom:8,
+    paddingLeft:20
+  },
+  itemTitle:{
+    color:'#909291',
+    fontSize:16
+  },
+  itemInfo:{
+    borderTopWidth:1,
+    borderTopColor:'#dbdbdb',
+    paddingLeft:20,
+    backgroundColor:'#fff'
+  },
+  itemLi:{
+    height:50,
+    borderBottomWidth:1,
+    borderBottomColor:'#dbdbdb',
+    flexDirection:'row',
+    alignItems:'center'
+  },
+  itemLabel:{
+    fontSize:18,
+    width:110,
+    color:'#999'
+  },
+  itemValue:{
+    color:'#707070',
+    fontSize:18
   }
+
 });
 
 export default styleProfile;
